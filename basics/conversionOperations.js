9b9cedbbaf64ac5b9a07aed84c2625383ef88482
Umesh1703 <pathipatiumesh@gmail.com>
2025-07-27 06:38:09 +0000

    conversion operations

---
+++ basics/conversionOperations.js
@@ -0,0 +1,75 @@
+// let score = 33;
+// console.log(typeof(score));  => gives "number" as type
+
+// let score = "33";
+// let score = "33aa";
+// let score = null;
+// let score = undefined;
+// let score = true;
+// console.log(typeof(score));
+
+// let number = Number(score);
+// console.log(typeof(number));
+
+// console.log(number);
+
+//*******************************************
+// "33" => 33
+// "33aa" => NaN
+// "null" => NaN
+// undefined => NaN
+// true => 1; false => 0
+//*******************************************
+
+
+// let loggedIn = 1;
+// let loggedIn = 0;
+// let loggedIn = "";
+// let loggedIn = "Umesh";
+
+// let booleanValue = Boolean(loggedIn);
+// console.log(booleanValue);
+
+
+//*******************************************
+// 1 => true; 0 => false
+// "Umesh" => true; "" => false
+//*******************************************
+
+
+
+// let score = 33;
+// let string = String(score);
+
+// console.log(string);
+// console.log(typeof(string));
+
+
+
+
+
+
+//**************************************** Operations *********************************************************/
+
+// let number = 9;
+// let value = -number;
+// console.log(value);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "Hello";
+// let str2 = " Umesh";
+
+// console.log(str1 + str2);
+
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
