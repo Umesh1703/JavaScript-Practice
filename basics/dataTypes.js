6dcb376943d652f1b352b83121cb81d4bb979032
Umesh1703 <pathipatiumesh@gmail.com>
2025-07-27 05:58:55 +0000

    dataTypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,15 @@
+"use strict";   //Treats all JS code as the newer version
+
+let name = "Umesh";
+let age = 21;
+let loggedIn = true;
+
+// number
+// bigInt
+// String
+// boolean
+// null => standalone value
+// undefined
+// symbol => unique
+
+console.log(typeof(null));  // This returns the "object" as the type
